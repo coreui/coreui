ceec3c213a70e320670ca723a8c5fb15f9727040
Łukasz Holeczek <lukasz@holeczek.pl>
2018-04-16 17:41:17 +0200

    refactor: add error handling

--- js/src/utilities/hex-to-rgb.js
+++ js/src/utilities/hex-to-rgb.js
@@ -7,13 +7,27 @@
 
 /* eslint-disable no-magic-numbers */
 const hexToRgb = (color) => {
-  const hex = color.replace('#', '')
-  const r = parseInt(hex.substring(0, 2), 16)
-  const g = parseInt(hex.substring(2, 4), 16)
-  const b = parseInt(hex.substring(4, 6), 16)
+  if (typeof color === 'undefined') {
+    throw new Error('Hex color is not defined')
+  }
+  const hex = color.match(/^#(?:[0-9a-f]{3}){1,2}$/i)
+  if (!hex) {
+    throw new Error(`${color} is not a valid hex color`)
+  }
+  let r
+  let g
+  let b
+  if (color.length === 7) {
+    r = parseInt(color.substring(1, 3), 16)
+    g = parseInt(color.substring(3, 5), 16)
+    b = parseInt(color.substring(5, 7), 16)
+  } else {
+    r = parseInt(color.substring(1, 2), 16)
+    g = parseInt(color.substring(2, 3), 16)
+    b = parseInt(color.substring(3, 5), 16)
+  }
 
-  const result = `rgba(${r}, ${g}, ${b}`
-  return result
+  return `rgba(${r}, ${g}, ${b}`
 }
 
 export default hexToRgb
