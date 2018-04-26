032af3b322ae975d30197b1a3dfeeaa4a45d19bf
Łukasz Holeczek <lukasz@holeczek.pl>
2018-04-26 20:53:12 +0200

    fix: ie11-custom-properties

--- dist/js/coreui.js
+++ dist/js/coreui.js
@@ -575,13 +575,43 @@
    * Licensed under MIT (https://coreui.io/license)
    * --------------------------------------------------------------------------
    */
+  var getCssCustomProperties = function getCssCustomProperties() {
+    var cssCustomProperties = {};
+    var stylesheets = new Map(Object.entries(document.styleSheets));
+    stylesheets.forEach(function (stylesheet) {
+      var rules = new Map(Object.entries(stylesheet.cssRules));
+      rules.forEach(function (rule) {
+        if (rule.selectorText === '.ie-custom-properties') {
+          rule.style.cssText.split('; ').forEach(function (property) {
+            var name = property.split(': ')[0];
+            var value = property.split(': ')[1];
+            cssCustomProperties["--" + name] = value;
+          });
+        }
+      });
+    });
+    return cssCustomProperties;
+  };
+
+  var isIE11 = function isIE11() {
+    return Boolean(window.MSInputMethodContext) && Boolean(document.documentMode);
+  };
+
+  var isCustomProperty = function isCustomProperty(property) {
+    return property.match(/^--.*/i);
+  };
 
   var getStyle = function getStyle(property, element) {
     if (element === void 0) {
       element = document.body;
     }
 
-    return window.getComputedStyle(element, null).getPropertyValue(property).replace(/^\s/, '');
+    if (isCustomProperty(property) && isIE11) {
+      var cssCustomProperties = getCssCustomProperties();
+      return cssCustomProperties[property];
+    } else {
+      return window.getComputedStyle(element, null).getPropertyValue(property).replace(/^\s/, '');
+    }
   };
 
   /**
