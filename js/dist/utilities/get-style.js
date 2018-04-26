3219963bbbbdd10d6402388ed5e85deea346d9a1
Łukasz Holeczek <lukasz@holeczek.pl>
2018-04-26 20:24:31 +0200

    fix: ie11-custom-properties

--- js/dist/utilities/get-style.js
+++ js/dist/utilities/get-style.js
@@ -4,6 +4,28 @@
  * Licensed under MIT (https://coreui.io/license)
  * --------------------------------------------------------------------------
  */
+var getCssCustomProperties = function getCssCustomProperties() {
+  var cssCustomProperties = {};
+  var stylesheets = new Map(Object.entries(document.styleSheets));
+  stylesheets.forEach(function (stylesheet) {
+    var rules = new Map(Object.entries(stylesheet.cssRules));
+    rules.forEach(function (rule) {
+      if (rule.selectorText === ':root') {
+        rule.style.cssText.split('; ').forEach(function (property) {
+          var name = property.split(': ')[0];
+          var value = property.split(': ')[1];
+          cssCustomProperties[name] = value;
+        });
+      }
+    });
+  });
+  return cssCustomProperties;
+};
+
+var isIE11 = function isIE11() {
+  return Boolean(window.MSInputMethodContext) && Boolean(document.documentMode);
+};
+
 var getStyle = function getStyle(property, element) {
   if (element === void 0) {
     element = document.body;
