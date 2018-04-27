079757d9e171f2bdbf2465154acc4c085bf459f4
xidedix <xidedix@users.noreply.github.com>
2018-04-27 20:17:49 +0200

    chore: rollup utilities bundle

---
+++ build/rollup.config.utilities.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const path    = require('path')
+const babel   = require('rollup-plugin-babel')
+const resolve = require('rollup-plugin-node-resolve')
+
+const pkg     = require(path.resolve(__dirname, '../package.json'))
+const BUNDLE  = process.env.BUNDLE === 'true'
+const year    = new Date().getFullYear()
+
+let fileDest  = 'utilities.js'
+const plugins = [
+  babel({
+    exclude: 'node_modules/**', // Only transpile our source code
+    externalHelpersWhitelist: [ // Include only required helpers
+      'defineProperties',
+      'createClass',
+      'inheritsLoose',
+      'objectSpread'
+    ]
+  })
+]
+
+if (BUNDLE) {
+  fileDest = 'utilities.bundle.js'
+  // Remove last entry in external array to bundle Popper
+  external.pop()
+  plugins.push(resolve())
+}
+
+export default {
+  input: path.resolve(__dirname, '../js/src/utilities/index.js'),
+  output: {
+    banner: `/*!
+  * CoreUI v${pkg.version} (${pkg.homepage})
+  * Copyright ${year} ${pkg.author.name}
+  * Licensed under MIT (${pkg.homepage})
+  */`,
+    file: path.resolve(__dirname, `../dist/js/${fileDest}`),
+    format: 'umd',
+    name: 'utilities'
+  },
+  plugins
+}
