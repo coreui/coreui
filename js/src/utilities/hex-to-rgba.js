5ff1c403cc1aa00af73b7f34ffba5a1a146c0741
Łukasz Holeczek <lukasz@holeczek.pl>
2018-05-08 19:04:18 +0200

    Ship v2.0.0

--- js/src/utilities/hex-to-rgba.js
+++ js/src/utilities/hex-to-rgba.js
@@ -1,6 +1,6 @@
 /**
  * --------------------------------------------------------------------------
- * CoreUI Utilities (v2.0.0-rc.2): hex-to-rgba.js
+ * CoreUI Utilities (v2.0.0): hex-to-rgba.js
  * Licensed under MIT (https://coreui.io/license)
  * --------------------------------------------------------------------------
  */
