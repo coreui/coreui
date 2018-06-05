c502e98facf57b8ffacb2f345aa679b58ad16618
xidedix <xidedix@users.noreply.github.com>
2018-06-05 17:11:20 +0200

    refactor(get-styles): fix IE issues, optimize

--- js/dist/utilities/get-style.js
+++ js/dist/utilities/get-style.js
@@ -1,28 +1,38 @@
 /**
  * --------------------------------------------------------------------------
- * CoreUI Utilities (v2.0.1): get-style.js
+ * CoreUI Utilities (v2.0.2): get-style.js
  * Licensed under MIT (https://coreui.io/license)
  * --------------------------------------------------------------------------
  */
 var getCssCustomProperties = function getCssCustomProperties() {
   var cssCustomProperties = {};
-  var root = Object.entries(document.styleSheets).filter(function (value) {
-    return value[1].cssText.substring(0, ':root'.length) === ':root';
-  });
+  var sheets = document.styleSheets;
+  var cssText = '';
 
-  if (root.length === 0) {
-    root = Object.entries(document.styleSheets);
+  for (var i = sheets.length - 1; i > -1; i--) {
+    var rules = sheets[i].cssRules;
+
+    for (var j = rules.length - 1; j > -1; j--) {
+      if (rules[j].selectorText === '.ie-custom-properties') {
+        cssText = rules[j].cssText;
+        break;
+      }
+    }
+
+    if (cssText) {
+      break;
+    }
   }
 
-  var rule = Object.entries(root[0][1].cssRules).filter(function (value) {
-    return value[1].selectorText === '.ie-custom-properties';
-  });
-  var cssText = rule[0][1].style.cssText;
+  cssText = cssText.substring(cssText.lastIndexOf('{') + 1, cssText.lastIndexOf('}'));
   cssText.split(';').forEach(function (property) {
     if (property) {
       var name = property.split(': ')[0];
       var value = property.split(': ')[1];
-      cssCustomProperties["--" + name.trim()] = value.trim();
+
+      if (name && value) {
+        cssCustomProperties["--" + name.trim()] = value.trim();
+      }
     }
   });
   return cssCustomProperties;
@@ -54,18 +64,4 @@ var getStyle = function getStyle(property, element) {
 
   return style;
 };
-
-if (!Object.entries) {
-  Object.entries = function (obj) {
-    var ownProps = Object.keys(obj);
-    var i = ownProps.length;
-    var resArray = new Array(i);
-
-    while (i--) {
-      resArray[i] = [ownProps[i], obj[ownProps[i]]];
-    }
-
-    return resArray;
-  };
-}
 //# sourceMappingURL=get-style.js.map
