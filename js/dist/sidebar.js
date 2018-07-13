b913d6f091fccdab0f2117283e79b284014bc075
Łukasz Holeczek <lukasz@holeczek.pl>
2018-07-13 17:10:41 +0200

    Ship v2.0.4

--- js/dist/sidebar.js
+++ js/dist/sidebar.js
@@ -4,7 +4,7 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
 
 /**
  * --------------------------------------------------------------------------
- * CoreUI (v2.0.3): sidebar.js
+ * CoreUI (v2.0.4): sidebar.js
  * Licensed under MIT (https://coreui.io/license)
  * --------------------------------------------------------------------------
  */
@@ -15,7 +15,7 @@ var Sidebar = function ($) {
    * ------------------------------------------------------------------------
    */
   var NAME = 'sidebar';
-  var VERSION = '2.0.3';
+  var VERSION = '2.0.4';
   var DATA_KEY = 'coreui.sidebar';
   var EVENT_KEY = "." + DATA_KEY;
   var DATA_API_KEY = '.data-api';
