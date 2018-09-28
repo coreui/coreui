92e680804c21ba6a2cfd6c10931d51ef4f559bc1
xidedix <xidedix@users.noreply.github.com>
2018-09-28 15:38:25 +0200

    Ship: v2.0.12

--- dist/js/coreui-utilities.js
+++ dist/js/coreui-utilities.js
@@ -1,5 +1,5 @@
 /*!
-  * CoreUI v2.0.11 (https://coreui.io)
+  * CoreUI v2.0.12 (https://coreui.io)
   * Copyright 2018 Łukasz Holeczek
   * Licensed under MIT (https://coreui.io)
   */
@@ -11,7 +11,7 @@
 
   /**
    * --------------------------------------------------------------------------
-   * CoreUI Utilities (v2.0.11): get-style.js
+   * CoreUI Utilities (v2.0.12): get-style.js
    * Licensed under MIT (https://coreui.io/license)
    * --------------------------------------------------------------------------
    */
@@ -78,7 +78,7 @@
 
   /**
    * --------------------------------------------------------------------------
-   * CoreUI Utilities (v2.0.11): hex-to-rgb.js
+   * CoreUI Utilities (v2.0.12): hex-to-rgb.js
    * Licensed under MIT (https://coreui.io/license)
    * --------------------------------------------------------------------------
    */
@@ -114,7 +114,7 @@
 
   /**
    * --------------------------------------------------------------------------
-   * CoreUI Utilities (v2.0.11): hex-to-rgba.js
+   * CoreUI Utilities (v2.0.12): hex-to-rgba.js
    * Licensed under MIT (https://coreui.io/license)
    * --------------------------------------------------------------------------
    */
@@ -154,7 +154,7 @@
 
   /**
    * --------------------------------------------------------------------------
-   * CoreUI (v2.0.11): rgb-to-hex.js
+   * CoreUI (v2.0.12): rgb-to-hex.js
    * Licensed under MIT (https://coreui.io/license)
    * --------------------------------------------------------------------------
    */
