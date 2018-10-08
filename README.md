51748fd375937dab113d2242b165c9cfd31bcea4
xidedix <xidedix@users.noreply.github.com>
2018-10-08 14:56:09 +0200

    Ship: v2.0.15

--- README.md
+++ README.md
@@ -52,13 +52,13 @@ $ npm install @coreui/coreui --save
 ### Yarn
 
 ``` bash
-$ yarn add @coreui/coreui@2.0.14
+$ yarn add @coreui/coreui@2.0.15
 ```
 
 ### Composer
 
 ``` bash
-$ composer require coreui/coreui:2.0.14
+$ composer require coreui/coreui:2.0.15
 ```
 
 ## Usage
--- js/dist/utilities/get-style.js
+++ js/dist/utilities/get-style.js
@@ -1,6 +1,6 @@
 /**
  * --------------------------------------------------------------------------
- * CoreUI Utilities (v2.0.14): get-style.js
+ * CoreUI Utilities (v2.0.15): get-style.js
  * Licensed under MIT (https://coreui.io/license)
  * --------------------------------------------------------------------------
  */
