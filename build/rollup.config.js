05780ab04eeb8632b8f1d3e9d2e2629eb93a9e5e
mrholek <lukasz@holeczek.pl>
2019-03-19 17:00:11 +0100

    update rollup config

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -5,21 +5,48 @@ const babel   = require('rollup-plugin-babel')
 const resolve = require('rollup-plugin-node-resolve')
 const banner  = require('./banner.js')
 
-const BUNDLE  = process.env.BUNDLE === 'true'
+const BUNDLE = process.env.BUNDLE === 'true'
+const ESM = process.env.ESM === 'true'
 
-let fileDest  = 'coreui.js'
+let fileDest  = `coreui${ESM ? '.esm' : ''}`
 const external = ['jquery', 'perfect-scrollbar', 'popper.js']
 const plugins = [
-  babel({
-    exclude: 'node_modules/**', // Only transpile our source code
-    externalHelpersWhitelist: [ // Include only required helpers
-      'defineProperties',
-      'createClass',
-      'inheritsLoose',
-      'defineProperty',
-      'objectSpread'
-    ]
-  })
+  babel(ESM ?
+    {
+      // Only transpile our source code
+      exclude: 'node_modules/**',
+      babelrc: false,
+      presets: [
+        [
+          '@babel/env',
+          {
+            loose: true,
+            modules: false,
+            targets: {
+              browsers: [
+                'Chrome >= 60',
+                'Safari >= 10.1',
+                'iOS >= 10.3',
+                'Firefox >= 54',
+                'Edge >= 15'
+              ]
+            }
+          }
+        ]
+      ]
+    } :
+    {
+      // Only transpile our source code
+      exclude: 'node_modules/**',
+      // Include only required helpers
+      externalHelpersWhitelist: [
+        'defineProperties',
+        'createClass',
+        'inheritsLoose',
+        'defineProperty',
+        'objectSpread'
+      ]
+    })
 ]
 const globals = {
   jquery: 'jQuery', // Ensure we use jQuery which is always available even in noConflict mode
@@ -28,8 +55,8 @@ const globals = {
 }
 
 if (BUNDLE) {
-  fileDest = 'coreui.bundle.js'
-  // Remove last entry in external array to bundle Popper and 
+  fileDest += '.bundle'
+  // Remove last entry in external array to bundle Popper and
   external.pop()
   external.pop()
   delete globals['popper.js']
@@ -37,15 +64,20 @@ if (BUNDLE) {
   plugins.push(resolve())
 }
 
-module.exports = {
-  input: path.resolve(__dirname, '../js/src/index.js'),
+const rollupConfig = {
+  input: path.resolve(__dirname, `../js/index.${ESM ? 'esm' : 'umd'}.js`),
   output: {
     banner,
-    file: path.resolve(__dirname, `../dist/js/${fileDest}`),
-    format: 'umd',
-    globals,
-    name: 'coreui'
+    file: path.resolve(__dirname, `../dist/js/${fileDest}.js`),
+    format: ESM ? 'esm' : 'umd',
+    globals
   },
   external,
   plugins
 }
+
+if (!ESM) {
+  rollupConfig.output.name = 'bootstrap'
+}
+
+module.exports = rollupConfig
