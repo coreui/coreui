9c5f98aee3be68aef9037a9cfbff3926d9259c30
Łukasz Holeczek <lukasz@holeczek.pl>
2021-01-21 00:21:09 +0100

    chore: bump version to v4.0.0-alpha.1

--- js/src/util/sanitizer.js
+++ js/src/util/sanitizer.js
@@ -1,6 +1,6 @@
 /**
  * --------------------------------------------------------------------------
- * CoreUI (v4.0.0-alpha.0): alert.js
+ * CoreUI (v4.0.0-alpha.1): alert.js
  * Licensed under MIT (https://coreui.io/license)
  *
  * This component is a modified version of the Bootstrap's  util/sanitizer.js
