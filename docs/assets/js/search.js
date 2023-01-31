fd97bf175eed19bd374b2964d9fa805254ad169e
Łukasz Holeczek <lukasz@holeczek.pl>
2023-01-31 14:32:45 +0100

    docs: add search

--- docs/assets/js/search.js
+++ docs/assets/js/search.js
@@ -11,36 +11,11 @@
     return
   }
 
-  const siteDocsVersion = searchElement.getAttribute('data-bd-docs-version')
-
   window.docsearch({
-    apiKey: '3151f502c7b9e9dafd5e6372b691a24e',
-    indexName: 'bootstrap',
-    appId: 'AK7KMZKZHQ',
+    appId: '5OOVC1SDJS',
+    apiKey: 'ab4149e82cfd175c0afe647a937a8d21',
+    indexName: 'coreui',
     container: searchElement,
-    searchParameters: {
-      facetFilters: [`version:${siteDocsVersion}`]
-    },
-    transformItems(items) {
-      return items.map(item => {
-        const liveUrl = 'https://getbootstrap.com/'
-
-        item.url = window.location.origin.startsWith(liveUrl) ?
-          // On production, return the result as is
-          item.url :
-          // On development or Netlify, replace `item.url` with a trailing slash,
-          // so that the result link is relative to the server root
-          item.url.replace(liveUrl, '/')
-
-        // Prevent jumping to first header
-        if (item.anchor === 'content') {
-          item.url = item.url.replace(/#content$/, '')
-          item.anchor = null
-        }
-
-        return item
-      })
-    },
     // Set debug to `true` if you want to inspect the dropdown
     debug: false
   })
