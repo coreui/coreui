98dbc78f3ca575633b078bebdc8a2898c3c3d325
Łukasz Holeczek <lukasz@holeczek.pl>
2023-01-31 14:33:25 +0100

    chore: update year to 2023

--- js/dist/popover.js
+++ js/dist/popover.js
@@ -1,6 +1,6 @@
 /*!
   * CoreUI popover.js v4.2.6 (https://coreui.io)
-  * Copyright 2022 The CoreUI Team (https://github.com/orgs/coreui/people)
+  * Copyright 2023 The CoreUI Team (https://github.com/orgs/coreui/people)
   * Licensed under MIT (https://coreui.io)
   */
 (function (global, factory) {
