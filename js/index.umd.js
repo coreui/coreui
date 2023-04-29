5c268b68aaebd6df5ef0e61876ad0ae8d2cc39d3
mrholek <lukasz@holeczek.pl>
2023-04-29 16:20:08 +0200

    refactor: add files extensios

--- js/index.umd.js
+++ js/index.umd.js
@@ -1,24 +1,24 @@
 /**
  * --------------------------------------------------------------------------
- * CoreUI (v4.2.6): index.esm.js
- * Licensed under MIT (https://coreui.io/license)
+ * CoreUI index.esm.js
+ * Licensed under MIT (https://github.com/coreui/coreui/blob/main/LICENSE)
  * --------------------------------------------------------------------------
  */
 
-import Alert from './src/alert'
-import Button from './src/button'
-import Carousel from './src/carousel'
-import Collapse from './src/collapse'
-import Dropdown from './src/dropdown'
-import Modal from './src/modal'
-import Navigation from './src/navigation'
-import OffCanvas from './src/offcanvas'
-import Popover from './src/popover'
-import ScrollSpy from './src/scrollspy'
-import Sidebar from './src/sidebar'
-import Tab from './src/tab'
-import Toast from './src/toast'
-import Tooltip from './src/tooltip'
+import Alert from './src/alert.js'
+import Button from './src/button.js'
+import Carousel from './src/carousel.js'
+import Collapse from './src/collapse.js'
+import Dropdown from './src/dropdown.js'
+import Modal from './src/modal.js'
+import Navigation from './src/navigation.js'
+import OffCanvas from './src/offcanvas.js'
+import Popover from './src/popover.js'
+import ScrollSpy from './src/scrollspy.js'
+import Sidebar from './src/sidebar.js'
+import Tab from './src/tab.js'
+import Toast from './src/toast.js'
+import Tooltip from './src/tooltip.js'
 
 export default {
   Alert,
