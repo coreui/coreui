e0466214241ed9d60325b81e7bc8229d96e5754e
mrholek <lukasz@holeczek.pl>
2023-11-14 16:14:15 +0100

    release: v5.0.0-rc.0

--- dist/js/coreui.bundle.js
+++ dist/js/coreui.bundle.js
@@ -1,5 +1,5 @@
 /*!
-  * CoreUI v5.0.0-beta.2 (https://coreui.io)
+  * CoreUI v5.0.0-rc.0 (https://coreui.io)
   * Copyright 2023 The CoreUI Team (https://github.com/orgs/coreui/people)
   * Licensed under MIT (https://github.com/coreui/coreui/blob/main/LICENSE)
   */
@@ -666,7 +666,7 @@
    * Constants
    */
 
-  const VERSION = '5.0.0-beta.2';
+  const VERSION = '5.0.0-rc.0';
 
   /**
    * Class definition
--- js/src/base-component.js
+++ js/src/base-component.js
@@ -17,7 +17,7 @@ import { executeAfterTransition, getElement } from './util/index.js'
  * Constants
  */
 
-const VERSION = '5.0.0-beta.2'
+const VERSION = '5.0.0-rc.0'
 
 /**
  * Class definition
