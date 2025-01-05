478d1cfc3a93b96fdb5b4e20cf2ec37e8e3fdfe7
mrholek <lukasz@holeczek.pl>
2025-01-05 14:17:11 +0100

    build: update `docs` action

--- js/dist/scrollspy.js
+++ js/dist/scrollspy.js
@@ -1,6 +1,6 @@
 /*!
   * CoreUI scrollspy.js v5.2.0 (https://coreui.io)
-  * Copyright 2024 The CoreUI Team (https://github.com/orgs/coreui/people)
+  * Copyright 2025 The CoreUI Team (https://github.com/orgs/coreui/people)
   * Licensed under MIT (https://github.com/coreui/coreui/blob/main/LICENSE)
   */
 (function (global, factory) {
