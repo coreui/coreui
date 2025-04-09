c77b0f312fc2c54d8952b0e130821993562ea85f
mrholek <lukasz@holeczek.pl>
2025-04-09 08:56:42 +0200

    docs: update links to Popper.js documentation

--- js/src/tooltip.js
+++ js/src/tooltip.js
@@ -108,7 +108,7 @@ const DefaultType = {
 class Tooltip extends BaseComponent {
   constructor(element, config) {
     if (typeof Popper === 'undefined') {
-      throw new TypeError('Bootstrap\'s tooltips require Popper (https://popper.js.org)')
+      throw new TypeError('CoreUI\'s dropdowns require Popper (https://popper.js.org/docs/v2/)')
     }
 
     super(element, config)
